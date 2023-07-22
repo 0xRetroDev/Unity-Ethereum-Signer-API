b84c5e96a7f41dedbfef6f63bb01d85218b9d532
nftpixels <weyers70@gmail.com>
2023-07-22 18:25:49 +1200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ethers } = require('ethers');
 const jwt = require('jsonwebtoken');
+const fetch = require('node-fetch');
 const contractABI = require('./contractABI');
-
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,10 +14,7 @@ const secretKey = 'YOUR_SECRET_KEY'; // Replace this with a secret key for JWT s
 
 
 // Endpoint to create a new wallet for a player
-app.post('/createWallet', (req, res) => {
-  console.log('Received request to create wallet.');
-  console.log('Request headers:', req.headers);
-  console.log('Request body:', req.body);
+app.post('/createWallet', authenticatePlayer, async (req, res) => {
   const wallet = ethers.Wallet.createRandom();
   const address = wallet.address;
   const privateKey = wallet.privateKey;
@@ -26,7 +23,13 @@ app.post('/createWallet', (req, res) => {
   // Generate a JWT token for the player
   const token = jwt.sign({ address }, secretKey);
 
-  res.json({ address, privateKey, token });
+  // Call the external API to claim gas for the wallet
+  try {
+    await claimGasForWallet(address);
+    res.json({ address, privateKey, token });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to claim gas for wallet', details: err.message });
+  }
 });
 
 // Endpoint to call the TokenCollected method
@@ -42,7 +45,7 @@ app.post('/tokenCollected', authenticatePlayer, async (req, res) => {
     const wallet = new ethers.Wallet(privateKey, provider);
 
     // Use the address and privateKey to interact with the smart contract and call TokenCollected method
-    const contractAddress = '0xC6633354CeB5Ed42cF26EA5F4a24DE7b833C8c86';
+    const contractAddress = '0x4A5B12722C57d48d3Cff9629E5B2039e11539cfd';
     const contractABI = contractABI;
     const contract = new ethers.Contract(contractAddress, contractABI, wallet);
 
@@ -56,9 +59,7 @@ app.post('/tokenCollected', authenticatePlayer, async (req, res) => {
 
 // Middleware to authenticate the player using the session token
 function authenticatePlayer(req, res, next) {
-  console.log('Verifying session token...');
   const token = req.header('x-session-token');
-  console.log('Received session token:', token);
 
   if (!token) {
     return res.status(401).json({ error: 'Authentication failed. Session token not provided.' });
@@ -74,9 +75,24 @@ function authenticatePlayer(req, res, next) {
   }
 }
 
+// Function to call the external API and claim gas for the wallet
+async function claimGasForWallet(walletAddress) {
+  const apiUrl = `https://gas-distribution.onrender.com/claim/${walletAddress}`;
+  
+  const response = await fetch(apiUrl, {
+    method: 'POST'
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to claim gas for wallet. API responded with status: ${response.status}`);
+  }
+
+  // Gas successfully claimed
+  return true;
+}
 
 // Start the server
-const port = 8888;
+const port = 3000;
 app.listen(port, () => {
-  console.log(`API server is running on ${port}`);
+  console.log(`API server is running on http://localhost:${port}`);
 });
